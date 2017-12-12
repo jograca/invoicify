fada9e1131101699f7ea1972f2c0af18cb81edfa
Jon Graca <jon.graca@gmail.com>
2017-12-12 10:33:07 -0500

    Initial Check-In

---
+++ src/main/java/com/lmig/gfc/invoicify/InvoicifyApplication.java
@@ -0,0 +1,12 @@
+package com.lmig.gfc.invoicify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicifyApplication.class, args);
+	}
+}
